458b8a9fd2c5eb311379995515c40339417bd1b2
sean comor <seancomor@gmail.com>
2023-01-25 19:40:21 +0100

    initial commit

---
+++ backend/main.py
@@ -0,0 +1,26 @@
+#main.py 
+
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from core.config import settings
+from apis.general_pages.route_homepage import general_pages_router
+from db.session import engine   #new
+from db.base_class import Base  #new
+
+
+def include_router(app):
+	app.include_router(general_pages_router)
+
+
+def create_tables():           #new
+	Base.metadata.create_all(bind=engine)
+
+	
+def start_application():
+	app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+	include_router(app)
+	#configure_static(app)
+	create_tables()       #new
+	return app
+
+app = start_application()
